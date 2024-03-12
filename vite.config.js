955012f9e7e4a2c8cf4064bcb2fa49d511ab1245
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2024-03-12 18:16:52 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/persona-/',
   build:{
     outDir:"./docs",
   }
