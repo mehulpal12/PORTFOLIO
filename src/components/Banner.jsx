e927946582dfc4dbfd4a3afa0382683e66e5eea1
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2024-03-15 19:52:25 +0530

    Banner done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,41 +1,66 @@
 import React, { useRef, useEffect } from 'react'
 import Typed from 'typed.js'
+import { LuGithub } from "react-icons/lu";
+import { CiLinkedin } from "react-icons/ci";
 
 function Banner() {
   const el = useRef(null)
 
-  useEffect(( ) =>{
-const typed = new Typed(el.current, {
-strings: ["FrontEnd Developer"], // Strings to display
-// Speed settings, try diffrent values untill you get good results
-startDelay: 300,
-typeSpeed: 100,
-backSpeed: 100,
-backDelay: 100,
-});
-// Destropying
-return() => {
-typed.destroy();
-};
-}, []);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["FrontEnd Developer"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+    });
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <>
-      <div className="main px-12 md:flex items-center">
-        <div className="text flex justify-center w-full">
-            <div className='w-2/3'>
+      <div className="main px-12 md:pt-16 md:flex bg-gray-100 items-center">
+        <div className="text flex justify-center md:justify-end w-full">
+          <div className='md:w-3/5 '>
             <h3 className='text-3xl font-semibold'>Hi, I am</h3>
             <h1 className='my-2 text-5xl font-bold'>Mehul Pal</h1>
             <h2 className='text-3xl '>A <span ref={el}></span></h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora fuga expedita aut sunt illum odit aliquam magnam. Adipisci, dolor exercitationem.</p>
             <br />
-            <a href="/contact" className='px-3 py-2 bg-orange-500 rounded-full shadow-lg'>Contact Me</a>
+            <div className="icons flex">
+              <CiLinkedin className='text-4xl cursor-pointer'/>
+              <LuGithub className='text-3xl mt-1 mx-5 cursor-pointer'/>
             </div>
+          </div>
         </div>
-        <div className="image mt-5 sm:mt-0 w-full flex justify-center">
-            <img src="https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/passport/1-change1.jpg" className='shadow w-fit' alt="" />
+        <div className="image mt-5 sm:mt-0 w-full flex sm:ms-36 justify-start">
+          <img src='./src/components/photo.jpg' className='hero-img rounded-full w-full sm:w-1/2 md:w-1/2 bg-cover' alt="" />
         </div>
       </div>
+        <div className="tech-stack  sm:flex text-center sm:ps-40 md:ps-72 sm:pt-32 bg-gray-100 py-10 pt-10">
+          <div className="text-xl font-semibold mt-2 mx-10 text-gray-700">
+            <h1>Tech-Stack ||</h1>
+          </div>
+          <div className="grid mt-5 sm:mt-0 ps-10 sm:ps-0 grid-cols-2 place-content-center sm:grid-cols-3 md:grid-cols-4 gap-4">
+            <div>
+              <img src="https://skillicons.dev/icons?i=html,css" className='cursor-pointer' alt="" />
+            </div>
+            <div>
+              <img src="https://skillicons.dev/icons?i=js,ts" className='cursor-pointer' alt="" />
+            </div>
+            <div>
+              <img src="https://skillicons.dev/icons?i=react,next" className='cursor-pointer' alt="" />
+            </div>
+            <div>
+              <img src="https://skillicons.dev/icons?i=tailwind,sass" className='cursor-pointer' alt="" />
+            </div>
+          </div>
+        </div>
+  
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
 
   return (
     <>
-     <div className="h-20 pt-5 sm:pt-0 px-20 bg-gray-100 items-center border main sm:flex sm:justify-between">
+     <div className="h-20 pt-5 sm:pt-0 px-20  items-center shadow-lg main sm:flex sm:justify-between">
         <div className="left">
-            <h1 className='text-2xl font-bold'>Mehul Pal</h1>
+            <h1 className='text-2xl font-bold hover:text-blue-400 cursor-pointer'>Mehul Pal</h1>
         </div>
         <div onClick={()=> setnav(!nav)} className="w-7 h-7 absolute right-8 top-6 cursor-pointer md:hidden">
         {
@@ -24,16 +24,13 @@ function Header() {
         }
         </div>
         <div className="center">
-        <ul className={` md:flex md:gap-5 text-center pe-8  md:items-center md:pb-0 pb-6 absolute md:static sm:bg-gray-100 bg-white md:z-auto z-[1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${nav ? 'top-20' : 'top-[-490px]'}`}>
+        <ul className={` md:flex md:gap-5 text-center pe-8  md:items-center md:pb-0 pb-6 absolute md:static text-xl font-semibold text-gray-900 bg-white md:z-auto z-[1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${nav ? 'top-20' : 'top-[-490px]'}`}>
                 {menulinks.map((link)=>(
                     <li key={link.name}>
-                        <a href={link.link} className='hover:text-orange-600 '>{link.name}</a>
+                        <a href={link.link} className='hover:text-blue-400 '>{link.name}</a>
                     </li>
                 ))}
             </ul>
-        </div>
-        <div className="right hidden sm:block">
-            <button className='px-3 py-2 bg-orange-600 rounded-full shadow text-xl'>Hire Me</button>
         </div>
      </div>
     </>
